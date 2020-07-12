c34f11c66c5b000795f43ce11ca12d8277c9f156
nagavineerpasam <nagavineerpasam@gmail.com>
2020-07-12 16:17:04 +0530

    Updates

---
+++ precache-manifest.b5ffbf577ded3c8a155709e891884072.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0513ce0e48216db6a281cad935ad96d",
+    "url": "/Corona-virus-tracker/index.html"
+  },
+  {
+    "revision": "15b45a2863d3e5bc6b80",
+    "url": "/Corona-virus-tracker/static/css/main.c6e6e723.chunk.css"
+  },
+  {
+    "revision": "5c5169ed356181213490",
+    "url": "/Corona-virus-tracker/static/js/2.1aa5c894.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-virus-tracker/static/js/2.1aa5c894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b45a2863d3e5bc6b80",
+    "url": "/Corona-virus-tracker/static/js/main.ca998e8d.chunk.js"
+  },
+  {
+    "revision": "306dde32dbf36eb5ad78",
+    "url": "/Corona-virus-tracker/static/js/runtime-main.4b17da5e.js"
+  },
+  {
+    "revision": "89f22d6dfea293905e86ae6abeefc8f4",
+    "url": "/Corona-virus-tracker/static/media/to.89f22d6d.jpg"
+  }
+]);
